7eb2cb568f9ba2e90eae88b6b7c7d2d2d6bc1166
BrolyDBZ <sanketskbtm61@gmail.com>
2023-05-01 16:46:59 +0530

    update to portfolio

--- src/components/nav.js
+++ src/components/nav.js
@@ -173,7 +173,12 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a
+      className="resume-button"
+      href="/SanketBondre-Resume.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       Resume
     </a>
   );
