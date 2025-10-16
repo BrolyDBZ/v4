35e88c7c6263aabafae8003c88c191af5cc8b3ff
Sanket <80707825+BrolyDBZ@users.noreply.github.com>
2025-10-17 00:48:54 +0530

    Update head.js

--- src/components/head.js
+++ src/components/head.js
@@ -60,7 +60,7 @@ const Head = ({ title, description, image }) => {
       <meta name="twitter:image" content={seo.image} />
 
       <meta name="google-site-verification" content="DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk" />
-      <meta name="google-adsense-account" content="ca-pub-8863839448952414">
+      <meta name="google-adsense-account" content="ca-pub-8863839448952414" />
     </Helmet>
   );
 };
